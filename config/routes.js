3097f938cd533b385c3eb2e5e4907a6a3d4cba39
aurelieslod <aurelie.slod@gmail.com>
2017-06-08 17:47:27 +0200

    delete useless files

--- config/routes.js
+++ config/routes.js
@@ -4,13 +4,6 @@ module.exports = function(app){
 	var home = require('../app/controllers/home');
 	app.get('/', home.index);
 
-
-	var firstTest = require('../app/controllers/firstTest');
-	app.get('/firstTest', firstTest.index);
-
-	var articles = require('../app/controllers/articles');
-	app.get('/articles', articles.index);
-
 	var users = require('../app/controllers/users');
 	app.get('/users', users.index);
 
